38398bcdde48083d5883a4514166366b2e6e132b
Maxime @ Bytes 'n AI <maxime@bytesnai.tech>
2024-02-24 00:53:31 +0000

    Forking from SECEF project

---
+++ prewikka/utils/html/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from .escape import Markup, escape, escapejs
---
+++ prewikka/version.py
@@ -0,0 +1,8 @@
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("prewikka").version
+__version_info__ = __version__.split(".")
+__branch__ = "5.2"
+__author__ = "Prelude Team"
+__license__ = "BSD"
+__copyright__ = "CS GROUP - France"
